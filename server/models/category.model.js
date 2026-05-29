1aef151d74cf31b304587e49dc23b089cf7b1da1
Rawda Yasser <rawdaymohamed@gmail.com>
2026-05-30 02:36:10 +0300

    Feat: add indexes to user, product and category schemas

--- server/models/category.model.js
+++ server/models/category.model.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: [true, "Category name is required"],
-			unique: true,
-			trim: true,
-		},
-		slug: {
-			type: String,
-			required: true,
-			unique: true,
-			lowercase: true,
-			trim: true,
-		},
-		imageUrl: {
-			type: String,
-			required: [true, "Category image is required"],
-		},
-	},
-	{ timestamps: true }
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      required: [true, "Category image is required"],
+    },
+  },
+  { timestamps: true },
 );
 
+categorySchema.index({
+  name: 1,
+});
 const Category = mongoose.model("Category", categorySchema);
 
 export default Category;
