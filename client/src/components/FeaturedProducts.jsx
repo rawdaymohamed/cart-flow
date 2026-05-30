b89b69ace10ffe07742e43d382fc4fe37ef02ddd
Rawda Yasser <rawdaymohamed@gmail.com>
2026-05-30 20:48:37 +0300

    Fix: fix add product to cart in featured products login required

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -1,3 +1,5 @@
+import toast from "react-hot-toast";
+import { useUserStore } from "../stores/useUserStore";
 import { useEffect, useState } from "react";
 import { ShoppingCart, ChevronLeft, ChevronRight } from "lucide-react";
 import { useCartStore } from "../stores/useCartStore";
@@ -5,6 +7,7 @@ import { useCartStore } from "../stores/useCartStore";
 const FeaturedProducts = ({ featuredProducts }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(4);
+  const { user } = useUserStore();
 
   const { addToCart } = useCartStore();
   const safeFeaturedProducts = Array.isArray(featuredProducts)
@@ -35,6 +38,15 @@ const FeaturedProducts = ({ featuredProducts }) => {
   const isStartDisabled = currentIndex === 0;
   const isEndDisabled =
     currentIndex >= safeFeaturedProducts.length - itemsPerPage;
+  const handleAddToCart = (product) => {
+    if (!user) {
+      toast.error("Please login to add products to cart", { id: "login" });
+      return;
+    } else {
+      // add to cart
+      addToCart(product);
+    }
+  };
 
   return (
     <div className="py-12">
@@ -71,7 +83,7 @@ const FeaturedProducts = ({ featuredProducts }) => {
                         ${product.price.toFixed(2)}
                       </p>
                       <button
-                        onClick={() => addToCart(product)}
+                        onClick={() => handleAddToCart(product)}
                         className="flex items-center justify-center w-full px-4 py-2 font-semibold transition-colors duration-300 rounded bg-accent hover:bg-accentHover text-ink"
                       >
                         <ShoppingCart className="w-5 h-5 mr-2" />
